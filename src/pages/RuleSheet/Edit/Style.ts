5fdfaa6fd4d0d7d327c9866a1398aed9a70b2fc0
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-04-20 21:36:02 -0300

    feat: Edit Rule Sheet basic form

--- src/pages/RuleSheet/Edit/Style.ts
+++ src/pages/RuleSheet/Edit/Style.ts
@@ -6,6 +6,32 @@ const Style = makeStyles(theme => ({
     paddingTop: 16,
     paddingBottom: 16,
   },
+  headingContainer: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  headingButtonsContainer: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  form: {
+    padding: 16,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  actionButtonsContainer: {
+    marginTop: 16,
+  },
+  loadingRecord: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
 }));
 
 export default Style;
