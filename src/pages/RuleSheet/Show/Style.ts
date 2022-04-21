6d888985af966dc3a68c60040ffe8d14c9f15106
C1320805 Elias Feijo de Almeida Pereira Lima <elias.lima@capgemini.com>
2022-04-20 22:13:00 -0300

    feat: Show Rule Sheet basic fields

--- src/pages/RuleSheet/Show/Style.ts
+++ src/pages/RuleSheet/Show/Style.ts
@@ -6,6 +6,38 @@ const Style = makeStyles(theme => ({
     paddingTop: 16,
     paddingBottom: 16,
   },
+  headingContainer: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  headingButtonsContainer: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  fieldsContainer: {
+    padding: 16,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  field: {
+    marginBottom: 16,
+  },
+  fieldLabel: {
+    fontWeight: 700,
+  },
+  fieldValue: {
+    marginLeft: 8,
+  },
+  actionButtonsContainer: {
+    marginTop: 16,
+  },
+  loadingRecord: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
 }));
 
 export default Style;
