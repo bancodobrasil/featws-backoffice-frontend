6a90dfde4684666585d4df206be85f288661dc3d
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-05-16 14:46:31 -0300

    feat: setting default baseURL value if the APP_URL env var is not provided (dev API URL)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { API_URL } from '../constants';
 
+const baseURL = API_URL || 'http://featws-api.cfe.desenv.bb.com.br/api/v1/rulesheets';
 // Axios instance used for all public routes of the API
-const publicAPI = axios.create({ baseURL: API_URL });
+const publicAPI = axios.create({ baseURL });
 publicAPI.defaults.headers.common['Content-Type'] = 'application/json;charset=UTF-8';
 
 export { publicAPI };
