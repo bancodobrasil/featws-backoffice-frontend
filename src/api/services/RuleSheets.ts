66c197a8647777d7771926ebb71002e381a37185
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-05-16 17:06:57 -0300

    feat: api/services/RuleSheets createRuleSheet method

--- src/api/services/RuleSheets.ts
+++ src/api/services/RuleSheets.ts
@@ -3,12 +3,13 @@ import { publicAPI } from '../axios';
 import { IRuleSheet } from '../../interfaces';
 import { createFakeRuleSheet } from '../../utils/factory/FakeRuleSheet';
 import { APIError, UnhandledError } from '../errors';
+import { WithRequired } from '../../types';
 
-type GetAllRulesheetsResponse = Pick<IRuleSheet, 'id' | 'name'>[];
+type GetAllRuleSheetsResponse = Pick<IRuleSheet, 'id' | 'name'>[];
 
-const getAllRuleSheets = async () => {
+const getAllRuleSheets = async (): Promise<IRuleSheet[]> => {
   try {
-    const response = await publicAPI.get<GetAllRulesheetsResponse>('/v1/rulesheets');
+    const response = await publicAPI.get<GetAllRuleSheetsResponse>('/v1/rulesheets');
     const records: IRuleSheet[] = response.data.map(record => {
       const fakeData = createFakeRuleSheet();
       return { ...fakeData, ...record };
@@ -26,4 +27,24 @@ const getAllRuleSheets = async () => {
   }
 };
 
-export { getAllRuleSheets };
+type CreateRuleSheetParams = WithRequired<IRuleSheet, 'name' | 'slug'>;
+type CreateRuleSheetResponse = Pick<IRuleSheet, 'id' | 'name'>;
+
+const createRuleSheet = async (data: CreateRuleSheetParams): Promise<IRuleSheet> => {
+  try {
+    const response = await publicAPI.post<CreateRuleSheetResponse>('/v1/rulesheets', data);
+    const fakeData = createFakeRuleSheet();
+    return { ...fakeData, ...response.data };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      // TODO: check response status code, inspect the response data, and throw the correct error type
+      if (error.response?.status) {
+        throw new APIError(error.response.status);
+      }
+    }
+    console.error(error);
+    throw new UnhandledError(error);
+  }
+};
+
+export { getAllRuleSheets, createRuleSheet };
