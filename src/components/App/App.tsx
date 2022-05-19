2b3987635d8ff80e1666dff800a1a5bee9983080
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-05-19 08:17:51 -0300

    feat: set ptBRTheme conditionally

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,16 +4,16 @@ import { CssBaseline } from '@mui/material';
 import theme from '../../theme';
 import './AppStyles.css';
 import { AppRoutes } from './AppRoutes';
-import '../../i18n';
+import i18n from '../../i18n';
 import Loading from '../Loading';
 
 const App = () => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <Suspense fallback={<Loading />}>
+  <Suspense fallback={<Loading />}>
+    <ThemeProvider theme={theme(i18n.language)}>
+      <CssBaseline />
       <AppRoutes />
-    </Suspense>
-  </ThemeProvider>
+    </ThemeProvider>
+  </Suspense>
 );
 
 export default App;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -36,135 +36,7 @@ const ptBRMuiDataGridOverride = {
   },
 };
 
-const theme = createTheme(
-  {
-    palette,
-    typography: bbTypography,
-    components: {
-      MuiInputLabel: {
-        styleOverrides: {
-          shrink: {
-            backgroundColor: palette.white,
-            paddingRight: 4,
-          },
-        },
-      },
-      MuiLink: {
-        defaultProps: {
-          underline: 'none',
-        },
-      },
-      MuiBreadcrumbs: {
-        styleOverrides: {
-          separator: {
-            display: 'none',
-          },
-        },
-      },
-      MuiButton: {
-        styleOverrides: {
-          root: {
-            padding: '12px 27.5px',
-            fontWeight: 700,
-            fontSize: '14px',
-            lineHeight: '16px',
-            letterSpacing: '1.25px',
-          },
-        },
-      },
-      MuiAlert: {
-        styleOverrides: {
-          message: {
-            color: palette.text.primary,
-            fontSize: 16,
-            fontWeight: 400,
-          },
-          filled: {
-            '& .MuiAlert-action': {
-              color: 'rgba(0, 0, 0, 0.6)',
-            },
-          },
-          filledSuccess: {
-            backgroundColor: 'rgb(239, 248, 240)',
-            '& .MuiAlert-icon': {
-              color: palette.success.light,
-            },
-          },
-          filledError: {
-            backgroundColor: 'rgb(253, 237, 237)',
-            '& .MuiAlert-icon': {
-              color: palette.error.light,
-            },
-          },
-          filledInfo: {
-            backgroundColor: 'rgb(236, 246, 254)',
-            '& .MuiAlert-icon': {
-              color: palette.info.light,
-            },
-          },
-          filledWarning: {
-            backgroundColor: 'rgb(255, 246, 233)',
-            '& .MuiAlert-icon': {
-              color: palette.warning.light,
-            },
-          },
-        },
-      },
-      MuiAlertTitle: {
-        styleOverrides: {
-          root: {
-            fontSize: 16,
-          },
-        },
-      },
-      MuiDataGrid: {
-        styleOverrides: {
-          root: {
-            border: 'none',
-            '& .MuiDataGrid-main': {
-              '& .MuiDataGrid-columnHeaders': {
-                backgroundColor: 'rgba(0, 0, 0, 0.01)',
-                boxShadow: 'inset 0px -1px 0px #E0E0E0',
-                '& .MuiDataGrid-columnHeader': {
-                  padding: '0 10px 0 8px',
-                  '&:focus': {
-                    outline: 'none',
-                  },
-                  '&:focus-within': {
-                    outline: 'none',
-                  },
-                  '& .MuiDataGrid-columnHeaderTitleContainer': {
-                    padding: 0,
-                    '& .MuiDataGrid-columnHeaderTitle': {
-                      color: 'rgba(0, 0, 0, 0.6);',
-                      fontWeight: 700,
-                      fontSize: '15px',
-                      lineHeight: '24px',
-                    },
-                    '& .MuiDataGrid-columnSeparator': {
-                      display: 'none',
-                    },
-                  },
-                },
-              },
-              '& .MuiDataGrid-cell': {
-                borderColor: '#E0E0E0;',
-                color: 'black',
-                fontWeight: 300,
-                fontSize: '16px',
-                lineHeight: '24px',
-                letterSpacing: '0.5px',
-                padding: '0 8px',
-                '&:focus-within': {
-                  outline: 'none',
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+const ptBRTheme = [
   ptBR,
   {
     components: {
@@ -176,6 +48,139 @@ const theme = createTheme(
       MuiDataGrid: ptBRMuiDataGridOverride,
     },
   },
-);
+];
+
+const theme = language =>
+  createTheme(
+    {
+      palette,
+      typography: bbTypography,
+      components: {
+        MuiInputLabel: {
+          styleOverrides: {
+            shrink: {
+              backgroundColor: palette.white,
+              paddingRight: 4,
+            },
+          },
+        },
+        MuiLink: {
+          defaultProps: {
+            underline: 'none',
+          },
+        },
+        MuiBreadcrumbs: {
+          styleOverrides: {
+            separator: {
+              display: 'none',
+            },
+          },
+        },
+        MuiButton: {
+          styleOverrides: {
+            root: {
+              padding: '12px 27.5px',
+              fontWeight: 700,
+              fontSize: '14px',
+              lineHeight: '16px',
+              letterSpacing: '1.25px',
+            },
+          },
+        },
+        MuiAlert: {
+          styleOverrides: {
+            message: {
+              color: palette.text.primary,
+              fontSize: 16,
+              fontWeight: 400,
+            },
+            filled: {
+              '& .MuiAlert-action': {
+                color: 'rgba(0, 0, 0, 0.6)',
+              },
+            },
+            filledSuccess: {
+              backgroundColor: 'rgb(239, 248, 240)',
+              '& .MuiAlert-icon': {
+                color: palette.success.light,
+              },
+            },
+            filledError: {
+              backgroundColor: 'rgb(253, 237, 237)',
+              '& .MuiAlert-icon': {
+                color: palette.error.light,
+              },
+            },
+            filledInfo: {
+              backgroundColor: 'rgb(236, 246, 254)',
+              '& .MuiAlert-icon': {
+                color: palette.info.light,
+              },
+            },
+            filledWarning: {
+              backgroundColor: 'rgb(255, 246, 233)',
+              '& .MuiAlert-icon': {
+                color: palette.warning.light,
+              },
+            },
+          },
+        },
+        MuiAlertTitle: {
+          styleOverrides: {
+            root: {
+              fontSize: 16,
+            },
+          },
+        },
+        MuiDataGrid: {
+          styleOverrides: {
+            root: {
+              border: 'none',
+              '& .MuiDataGrid-main': {
+                '& .MuiDataGrid-columnHeaders': {
+                  backgroundColor: 'rgba(0, 0, 0, 0.01)',
+                  boxShadow: 'inset 0px -1px 0px #E0E0E0',
+                  '& .MuiDataGrid-columnHeader': {
+                    padding: '0 10px 0 8px',
+                    '&:focus': {
+                      outline: 'none',
+                    },
+                    '&:focus-within': {
+                      outline: 'none',
+                    },
+                    '& .MuiDataGrid-columnHeaderTitleContainer': {
+                      padding: 0,
+                      '& .MuiDataGrid-columnHeaderTitle': {
+                        color: 'rgba(0, 0, 0, 0.6);',
+                        fontWeight: 700,
+                        fontSize: '15px',
+                        lineHeight: '24px',
+                      },
+                      '& .MuiDataGrid-columnSeparator': {
+                        display: 'none',
+                      },
+                    },
+                  },
+                },
+                '& .MuiDataGrid-cell': {
+                  borderColor: '#E0E0E0;',
+                  color: 'black',
+                  fontWeight: 300,
+                  fontSize: '16px',
+                  lineHeight: '24px',
+                  letterSpacing: '0.5px',
+                  padding: '0 8px',
+                  '&:focus-within': {
+                    outline: 'none',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    ...(language === 'pt-BR' ? ptBRTheme : []),
+  );
 
 export default theme;
