eb5d76532b11f5238d3aaa0dd439d5c9266f7918
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-05-24 16:38:39 -0300

    refactor: getting i18n instance from useTranslation hook

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,27 @@
 import React, { Suspense } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import '../../i18n';
 import theme from '../../theme';
 import './AppStyles.css';
 import { AppRoutes } from './AppRoutes';
-import i18n from '../../i18n';
 import Loading from '../Loading';
 
-const App = () => (
-  <Suspense fallback={<Loading />}>
+const Pages = () => {
+  const { i18n } = useTranslation();
+
+  return (
     <ThemeProvider theme={theme(i18n.language)}>
       <CssBaseline />
       <AppRoutes />
     </ThemeProvider>
+  );
+};
+
+const App = () => (
+  <Suspense fallback={<Loading />}>
+    <Pages />
   </Suspense>
 );
 
