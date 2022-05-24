80bc0a0b4329103523ace55102444bfed87b0d31
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-05-24 18:01:15 -0300

    feat: WrapPromise util to be used with React.Suspense

---
+++ src/utils/suspense/WrapPromise.ts
@@ -0,0 +1,37 @@
+enum Status {
+  PENDING = 'PENDING',
+  SUCCESSS = 'SUCCESS',
+  ERROR = 'ERROR',
+}
+
+// Helper method to be used with React.Suspense
+const wrapPromise = <T>(promise: Promise<T>): { read: () => T } => {
+  let status = Status.PENDING;
+  let response;
+
+  const suspender = promise.then(
+    res => {
+      status = Status.SUCCESSS;
+      response = res;
+    },
+    err => {
+      status = Status.ERROR;
+      response = err;
+    },
+  );
+
+  const read = (): T => {
+    switch (status) {
+      case Status.PENDING:
+        throw suspender;
+      case Status.ERROR:
+        throw response;
+      default:
+        return response;
+    }
+  };
+
+  return { read };
+};
+
+export default wrapPromise;
