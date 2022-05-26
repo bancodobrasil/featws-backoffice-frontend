486cb2678f9d29e7ac98ecc8ab3cd34b8f7887da
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-05-26 20:06:44 -0300

    refactor: pages/RuleSheet/List using React.Suspense and ErrorBoundary

--- src/pages/RuleSheet/List/List.tsx
+++ src/pages/RuleSheet/List/List.tsx
@@ -1,98 +1,24 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { Suspense, useMemo } from 'react';
 import { Box, Button, IconButton, Paper } from '@mui/material';
 import { DataGrid, GridColDef, GridSelectionModel } from '@mui/x-data-grid';
-import { useNavigate } from 'react-router-dom';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import { useTranslation } from 'react-i18next';
-import { NotificationContext, ActionTypes } from '../../../contexts/NotificationContext';
 import AuthorizedComponent from '../../../components/Auth/AuthorizedComponent';
-import { IRuleSheet } from '../../../interfaces';
 import { getAllRuleSheets } from '../../../api/services/RuleSheets';
 import Loading from '../../../components/Loading';
+import ErrorBoundary from '../../../components/ErrorBoundary';
 
-export const ListRuleSheet = () => {
-  const { t } = useTranslation();
+const resource = getAllRuleSheets();
 
-  const { dispatch } = useContext(NotificationContext);
-  const navigate = useNavigate();
-
-  const [records, setRecords] = useState<IRuleSheet[] | undefined>();
-  const [loadingRecords, setLoadingRecords] = useState<boolean>(false);
-  const [finishedLoading, setFinishedLoading] = useState<boolean>(false);
-
-  const columns: GridColDef[] = [
-    {
-      field: 'name',
-      headerName: t('rulesheet.of', { field: 'name' }),
-      minWidth: 300,
-    },
-    {
-      field: 'responsible',
-      headerName: t('rulesheet.fields.responsible'),
-      minWidth: 200,
-    },
-    {
-      field: 'code',
-      headerName: t('rulesheet.fields.code'),
-      minWidth: 150,
-    },
-    {
-      field: 'updatedAt',
-      type: 'date',
-      headerName: t('rulesheet.fields.updatedAt'),
-      minWidth: 220,
-    },
-    {
-      field: '',
-      headerName: '',
-      minWidth: 150,
-      sortable: false,
-      filterable: false,
-      disableColumnMenu: true,
-      align: 'right',
-      flex: 1,
-      renderCell: params => {
-        const onArrowIconClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-          e.stopPropagation();
-          e.preventDefault();
-          navigate(params.id.toString());
-        };
-
-        return (
-          <IconButton onClick={onArrowIconClick}>
-            <ArrowForwardIosRoundedIcon fontSize="small" />
-          </IconButton>
-        );
-      },
-    },
-  ];
-
-  const fetchRecords = useCallback(async () => {
-    if (loadingRecords) {
-      return;
-    }
-    setLoadingRecords(true);
-
-    try {
-      const data = await getAllRuleSheets();
-      setRecords(data);
-    } catch (error) {
-      dispatch({ type: ActionTypes.OPEN_ERROR_NOTIFICATION, error });
-    }
-
-    setLoadingRecords(false);
-    setFinishedLoading(true);
-  }, [loadingRecords, dispatch]);
-
-  useEffect(() => {
-    if (!finishedLoading) {
-      fetchRecords();
-    }
-  }, [finishedLoading, fetchRecords]);
-
-  const handleButtonCreateOnClick = () => {
-    navigate('create');
-  };
+const DataGridWrapper = ({
+  columns,
+  navigate,
+}: {
+  columns: GridColDef[];
+  navigate: NavigateFunction;
+}): JSX.Element => {
+  const records = resource.read();
 
   const onSelectionModelChangeHandler = (selectionModel: GridSelectionModel) => {
     if (selectionModel.length > 0) {
@@ -100,9 +26,80 @@ export const ListRuleSheet = () => {
     }
   };
 
-  if (loadingRecords) {
-    return <Loading />;
-  }
+  return (
+    <DataGrid
+      sx={{
+        '& .MuiDataGrid-main .MuiDataGrid-cell': {
+          cursor: 'pointer',
+        },
+      }}
+      rows={records}
+      columns={columns}
+      pageSize={10}
+      autoHeight
+      onSelectionModelChange={onSelectionModelChangeHandler}
+    />
+  );
+};
+
+export const ListRuleSheet = () => {
+  const { t } = useTranslation();
+
+  const navigate = useNavigate();
+
+  const columns: GridColDef[] = useMemo(
+    () => [
+      {
+        field: 'name',
+        headerName: t('rulesheet.of', { field: 'name' }),
+        minWidth: 300,
+      },
+      {
+        field: 'responsible',
+        headerName: t('rulesheet.fields.responsible'),
+        minWidth: 200,
+      },
+      {
+        field: 'code',
+        headerName: t('rulesheet.fields.code'),
+        minWidth: 150,
+      },
+      {
+        field: 'updatedAt',
+        type: 'date',
+        headerName: t('rulesheet.fields.updatedAt'),
+        minWidth: 220,
+      },
+      {
+        field: '',
+        headerName: '',
+        minWidth: 150,
+        sortable: false,
+        filterable: false,
+        disableColumnMenu: true,
+        align: 'right',
+        flex: 1,
+        renderCell: params => {
+          const onArrowIconClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+            e.stopPropagation();
+            e.preventDefault();
+            navigate(params.id.toString());
+          };
+
+          return (
+            <IconButton onClick={onArrowIconClick}>
+              <ArrowForwardIosRoundedIcon fontSize="small" />
+            </IconButton>
+          );
+        },
+      },
+    ],
+    [t, navigate],
+  );
+
+  const handleButtonCreateOnClick = () => {
+    navigate('create');
+  };
 
   return (
     <Box
@@ -142,18 +139,13 @@ export const ListRuleSheet = () => {
         }}
         elevation={0}
       >
-        <DataGrid
-          sx={{
-            '& .MuiDataGrid-main .MuiDataGrid-cell': {
-              cursor: 'pointer',
-            },
-          }}
-          rows={records}
-          columns={columns}
-          pageSize={10}
-          autoHeight
-          onSelectionModelChange={onSelectionModelChangeHandler}
-        />
+        <ErrorBoundary
+          fallback={t('common.error.service.getAll', { resource: t('rulesheet.name') })}
+        >
+          <Suspense fallback={<Loading />}>
+            <DataGridWrapper columns={columns} navigate={navigate} />
+          </Suspense>
+        </ErrorBoundary>
       </Paper>
     </Box>
   );
