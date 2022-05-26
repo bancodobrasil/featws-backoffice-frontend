fd77648c4cf94543cbf1e1eac9124664f88b254b
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-05-26 20:05:24 -0300

    refactor: RuleSheets service getAllRuleSheets using wrapPromise util method

--- src/api/services/RuleSheets.ts
+++ src/api/services/RuleSheets.ts
@@ -3,29 +3,20 @@ import { publicAPI } from '../axios';
 import { IRuleSheet } from '../../interfaces';
 import { createFakeRuleSheet } from '../../utils/factory/FakeRuleSheet';
 import { APIError, UnhandledError } from '../errors';
+import wrapPromise from '../../utils/suspense/WrapPromise';
 
 type GetAllRuleSheetsResponse = Pick<IRuleSheet, 'id' | 'name'>[];
 
-const getAllRuleSheets = async (): Promise<IRuleSheet[]> => {
-  try {
-    const response = await publicAPI.get<GetAllRuleSheetsResponse>('/v1/rulesheets');
+const getAllRuleSheets = (): { read: () => IRuleSheet[] } => {
+  const promise = publicAPI.get<GetAllRuleSheetsResponse>('/v1/rulesheets').then(response => {
     const records: IRuleSheet[] = response.data.map(record => {
       const fakeData = createFakeRuleSheet();
       return { ...fakeData, ...record };
     });
     return records;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      // TODO: check response status code, inspect the response data, and throw the correct error type
-      if (error.response?.status) {
-        throw new APIError(error.response.status);
-      }
-    }
-    console.error(error);
-    throw new UnhandledError(error);
-  }
+  });
+  return wrapPromise(promise);
 };
-
 type CreateRuleSheetParams = Required<Pick<IRuleSheet, 'name' | 'slug'>>;
 type CreateRuleSheetResponse = Pick<IRuleSheet, 'id' | 'name'>;
 
